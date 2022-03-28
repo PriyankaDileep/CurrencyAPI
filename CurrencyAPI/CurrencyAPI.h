7b871ccf955be28e199c89b70638c7a44dba3c64
Priyanka <priyanka@entegy.com.au>
2022-03-28 12:50:04 +1000

    Initial commit

---
+++ CurrencyAPI/CurrencyAPI.h
@@ -0,0 +1,18 @@
+//
+//  CurrencyAPI.h
+//  CurrencyAPI
+//
+//  Created by Priyanka on 28/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CurrencyAPI.
+FOUNDATION_EXPORT double CurrencyAPIVersionNumber;
+
+//! Project version string for CurrencyAPI.
+FOUNDATION_EXPORT const unsigned char CurrencyAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurrencyAPI/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'CurrencyAPI' do
+ 
+
+  # Pods for CurrencyAPI
+
+  target 'CurrencyAPITests' do
+    # Pods for testing
+  end
+
+end
